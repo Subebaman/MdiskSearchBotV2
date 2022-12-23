cee6936ad6e1d3716e89b8425d14f9924b579ac2
Subebaman <106474658+Subebaman@users.noreply.github.com>
2022-12-23 18:36:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/leviofficiall/Mdisk12345bot.git /Mdisk12345bot
-cd /Mdisk12345bot
+git clone https://github.com/leviofficiall/MdiskSearchBotV2.git /MdiskSearchBotV2
+cd /MdiskSearchBotV2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
